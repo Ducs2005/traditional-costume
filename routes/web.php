0b15e1e45f4002e0d7c299bf09d4baae9996a802
hoangnguyenvku000 <161429688+hoangnguyenvku000@users.noreply.github.com>
2024-10-27 14:04:51 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -55,8 +55,8 @@ Route::prefix('admin')->group(function () {
     Route::get('/users', [UserController::class, 'index'])->name('admin.users.index');
     Route::get('/users/create', [UserController::class, 'create'])->name('admin.users.create');
     Route::post('/users', [UserController::class, 'store'])->name('admin.users.store');
-    Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('admin.users.edit'); // Route cho chỉnh sửa
-    Route::put('/users/{id}', [UserController::class, 'update'])->name('admin.users.update'); // Route cho cập nhật
+    Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('admin.users.edit');  
+    Route::put('/users/{id}', [UserController::class, 'update'])->name('admin.users.update');  
     Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('admin.users.destroy'); // Route cho xóa
 });
 
