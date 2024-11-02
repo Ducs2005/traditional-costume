56213dc0dc53a92580db3bbfd75db0a107b7bd99
hoangnguyenvku000 <161429688+hoangnguyenvku000@users.noreply.github.com>
2024-11-02 16:02:25 +0700

    Update create.blade.php

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -3,6 +3,17 @@
 @section('content')
 <div class="container">
     <h1 class="text-center">Thêm người dùng</h1>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <form action="{{ route('admin.users.store') }}" method="POST">
         @csrf
         <div class="form-group">
