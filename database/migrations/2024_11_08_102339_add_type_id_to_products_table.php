2f48fa966d050e05a5759e4ba6b13c42e40d563f
hoangnguyenvku000 <161429688+hoangnguyenvku000@users.noreply.github.com>
2024-11-08 23:07:33 +0700

    Add files via upload

---
+++ database/migrations/2024_11_08_102339_add_type_id_to_products_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->unsignedBigInteger('type_id')->nullable();
+            $table->foreign('type_id')->references('id')->on('types')->onDelete('set null');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign(['type_id']);
+            $table->dropColumn('type_id');
+        });
+    }
+    
+};
